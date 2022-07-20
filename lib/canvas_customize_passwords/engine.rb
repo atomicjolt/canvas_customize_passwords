8e9e4ee3183a6771d098de0197a735279837224d
bfcoder <jamescarbine@gmail.com>
2022-07-20 15:51:40 -0600

    Set defaults better and add password composition into policy

--- lib/canvas_customize_passwords/engine.rb
+++ lib/canvas_customize_passwords/engine.rb
@@ -14,12 +14,18 @@ module CanvasCustomizePasswords
 
       if ActiveRecord::Base.connection.table_exists?('plugin_settings') && Canvas::Plugin.find(:canvas_customize_passwords).enabled?
         plugin = PluginSetting.find_by(name: "canvas_customize_passwords")
+        min_length = plugin.settings[:min_length].present? ? plugin.settings[:min_length].to_i : 12
+        max_repeats = plugin.settings[:max_repeats].present? ? plugin.settings[:max_repeats].to_i : 2
+        max_sequence = plugin.settings[:max_sequence].present? ? plugin.settings[:max_sequence].to_i : 3
+        disallow_common_passwords = plugin.settings[:disallow_common_passwords].to_i == 1
+        enforce_password_composition_rules = plugin.settings[:enforce_password_composition_rules].to_i == 1
         Canvas::PasswordPolicy.define_singleton_method :default_policy do
           {
-            :max_repeats => plugin.settings[:max_repeats] || 2,
-            :max_sequence => plugin.settings[:max_sequence] || 3,
-            :disallow_common_passwords => (plugin.settings[:disallow_common_passwords] == 1) || true,
-            :min_length => plugin.settings[:min_length] || 12,
+            :max_repeats => max_repeats,
+            :max_sequence => max_sequence,
+            :disallow_common_passwords => disallow_common_passwords,
+            :min_length => min_length,
+            :enforce_password_composition_rules => enforce_password_composition_rules,
           }
         end
 
